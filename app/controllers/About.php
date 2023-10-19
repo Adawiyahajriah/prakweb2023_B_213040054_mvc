9faada7eecfec78ac3fa88668d7526e56e416c34
adawiyahajr <adawiyahajr@gmail.com>
2023-10-19 23:10:07 +0700

    6 Assets

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,21 +1,21 @@
 <?php
-
-class About extends Controllers{
+class About extends Controllers {
     public function index($nama = 'Adawiyah', $pekerjaan = 'Mahasiswa', $umur = 20) 
     {
         $data['nama'] = $nama;
         $data['pekerjaan'] = $pekerjaan;
         $data['umur'] = $umur;
-        $data['judul'] = 'About Me';
+        $data['judul'] = 'About';
         $this->views('templates/header', $data);
-        $this->views('home/index', $data);
+        $this->views('about/index', $data); // Ganti 'home/index' dengan 'about/index' jika itu nama yang benar
         $this->views('templates/footer');
     }
+    
     public function page()
     {
         $data['judul'] = 'Pages';
         $this->views('templates/header', $data);
-        $this->views('about/page');
+        $this->views('about/page', $data); // Ganti 'about/page' dengan nama yang sesuai
         $this->views('templates/footer');
     }
 }
--- app/init.php
+++ app/init.php
@@ -2,3 +2,5 @@
 
 require_once 'core/App.php';
 require_once 'core/Controllers.php';
+
+require_once 'core/constants.php';
